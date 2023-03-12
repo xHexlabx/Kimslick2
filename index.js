2f500feea52ba138d0711f11a825be097732cc56
Meaw_zap <118115127+xHexlabx@users.noreply.github.com>
2023-03-12 07:30:14 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, IntentsBitField, Partials } = require("discord.js");
+const WOK = require("wokcommands");
+const path = require("path");
+require("dotenv/config");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.DirectMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+  partials: [Partials.Channel],
+});
+
+client.on("ready", () => {
+  new WOK({
+    client,
+    commandsDir: path.join(__dirname, "commands"),
+  });
+});
+
+client.login(process.env.TOKEN);
